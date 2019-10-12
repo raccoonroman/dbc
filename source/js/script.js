20ee9fc5d04a5967ac7266d61b30c728ed597953
Роман <kromanjs@gmail.com>
2019-10-12 13:05:07 +0300

    full page

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,17 @@
 "use strict";
 
-// new fullpage('#fullpage', {
-//   anchors: ['page1', 'page2', 'page3', 'page4'],
-//   navigationTooltips: ['fullPage', 'Open', 'Easy', 'Touch'],
-//   css3: true,
-//   scrollingSpeed: 1000,
-//   navigation: true,
-//   slidesNavigation: true,
-//   responsiveHeight: 330,
-//   dragAndMove: true,
-//   dragAndMoveKey: 'YWx2YXJvdHJpZ28uY29tX0EyMlpISmhaMEZ1WkUxdmRtVT0wWUc=',
-//   controlArrows: false
-// });
+new fullpage('#fullpage', {
+  anchors: ['page1', 'page2', 'page3', 'page4'],
+  navigationTooltips: ['fullPage', 'Open', 'Easy', 'Touch'],
+  css3: true,
+  scrollingSpeed: 1000,
+  navigation: true,
+  slidesNavigation: true,
+  responsiveHeight: 330,
+  dragAndMove: true,
+  dragAndMoveKey: 'YWx2YXJvdHJpZ28uY29tX0EyMlpISmhaMEZ1WkUxdmRtVT0wWUc=',
+  controlArrows: false
+});
 
 var hamburger = document.querySelector(".hamburger");
 // var navMain = document.querySelector('.header-nav__list');
